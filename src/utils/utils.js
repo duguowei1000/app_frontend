d6c49efe58fe94808e4df9e01000ca5856de9458
Du Guowei <duguowei1000@gmail.com>
2022-04-16 17:34:47 +0800

    chore: auth details

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,4 +1,6 @@
-export const BACKEND =
+const BACKEND =
 	process.env.NODE_ENV === 'production'
 		? process.env.REACT_APP_BACKEND
 		: 'http://localhost:2000'; // process.env.REACT_APP_BACKEND
+
+export default BACKEND;
