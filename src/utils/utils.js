055319b78e3a6767a537bf183f4ac0f7f1784545
Mork <mawwwrk@gmail.com>
2022-04-22 10:51:57 +0800

    fix: tweakes auth

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,11 +1,11 @@
 const isProduction = process.env.NODE_ENV === 'production';
 
 const BACKEND = isProduction
-	? process.env.REACT_APP_BACKEND
+	? process.env.REACT_APP_BACKEND_STAGING
 	: 'http://localhost:2000';
 
 const FRONTEND = isProduction
-	? process.env.REACT_APP_BACKEND
+	? process.env.REACT_APP_FRONTEND
 	: 'http://localhost:3000';
 
 export { BACKEND, FRONTEND };
