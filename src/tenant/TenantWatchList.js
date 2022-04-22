f978013d25ca8736d76c042cd050145d9e15e0fd
Du Guowei <duguowei1000@gmail.com>
2022-04-22 18:49:19 +0800

    chore: test tenant watchlist =>> fetch apicall issues

--- src/tenant/TenantWatchList.js
+++ src/tenant/TenantWatchList.js
@@ -6,14 +6,14 @@ import { Link } from 'react-router-dom';
 //Grab Tenant's Id
 //Populate Tenant's Liked Listings based on id  //
 //Routing should be based on Id
-const tenantID = '62603024c55f82e765ede5e5';
+const tenantID = '626256bdbd0702716c26976c';
 
 function TenantWatchList() {
-	const [tenantDetails, setTenantDetails] = useState();
+	const [tenantDetails, setTenantDetails] = useState([]);
 	const [fullListings, setFullListings] = useState([]);
 	const [watchlist, setWatchList] = useState([]);
-	const [ListStatus, setListStatus] = useState(null);
-	const [TenantDetailStatus, setTenantDetailStatus] = useState(null);
+	const [ListStatus, setListStatus] = useState(false);
+	const [TenantDetailStatus, setTenantDetailStatus] = useState(false);
 	const [status, setStatus] = useState(null);
 	const [canList, setCanList] = useState(null);
 
@@ -30,17 +30,10 @@ function TenantWatchList() {
 			});
 	};
 	const fetchTenantDetails = () => {
-		fetch(urlcat(BACKEND, '/api/tenant/listings'))
+		fetch(urlcat(BACKEND, '/api/tenant'))
 			.then((response) => response.json())
 			.then((data) => {
-				data.filter((e) => {
-					// console.log('ids in Tdetails', e._id)
-					if (e._id === tenantID) {
-						setTenantDetails(e.favourites);
-						console.log('tenant details', e.favourites);
-					} else console.log('wrong tenant id');
-				});
-
+				setTenantDetails(data);
 				setTenantDetailStatus(true);
 			})
 			.catch((error) => {
@@ -50,28 +43,43 @@ function TenantWatchList() {
 	};
 
 	const setupWatchList = () => {
-		console.log(tenantDetails);
-		if (tenantDetails.length) {
+		// const tenantFavs = []
+		console.log(tenantDetails[0]);
+		console.log('tenantDetails1' + tenantDetails[0]);
+
+		const specificTenant = tenantDetails.filter((e) => {
+			if (e._id === tenantID) {
+				return true;
+			} else console.log('wrong tenant id');
+		});
+		const tenantFavs = specificTenant[0].favourites;
+		//console.log(`tenantFavs,${tenantFavs}`)
+		//console.log('>>>>setupwatchlist',tenantDetails);
+		if (tenantFavs.length) {
 			let matchedListing = fullListings.filter((e) => {
-				return tenantDetails.includes(e._id);
+				return tenantFavs.includes(e._id);
 			});
 
-			console.log(matchedListing);
+			//console.log("matchedlist",matchedListing);
 			setWatchList(matchedListing);
 		}
 	};
 
 	useEffect(() => {
-		setTenantDetailStatus(false);
-		setListStatus(false);
+		setCanList(false);
 		setStatus('loading');
 		fetchFullList();
 		fetchTenantDetails();
+		//console.log(`ListStatus${ListStatus} TenantDetailsStatus${TenantDetailStatus}`)
 	}, []);
 
 	useEffect(() => {
+		console.log(
+			`ListStatus${ListStatus} TenantDetailsStatus${TenantDetailStatus}`
+		);
 		if (ListStatus && TenantDetailStatus) {
-			console.log('tenant details', tenantDetails); //check tenant ID to be sure
+			// console.log(`ListStatus${ListStatus} TenantDetailsStatus${TenantDetailStatus}`)
+			//console.log('tenant details', tenantDetails); //check tenant ID to be sure
 			setupWatchList();
 			setStatus('success');
 			setCanList(true);
@@ -79,7 +87,7 @@ function TenantWatchList() {
 	}, [ListStatus, TenantDetailStatus]);
 
 	if (status === 'loading') {
-		return <div>Loading... or no watchList</div>;
+		return <div>Loading..... or no watchList</div>;
 	}
 	if (status === 'No Watchlist') {
 		return <div>No watchList</div>;
@@ -96,59 +104,58 @@ function TenantWatchList() {
 	return (
 		<>
 			{/* {console.log(status)}
-			{status === "No Watchlist" ? <div>no watchList</div> :<div>watchlist</div> }
-			{canList&& */}
-			<div className='listingList'>
-				<ul>
-					{watchlist.map((listing) => (
-						<li key={listing._id}>
-							<div className='listing'>
-								<div className='listingImage'>
-									{<img src={listing.image} height='300px' width='400px' />}
-								</div>
-								<div className='listingInfo'>
-									<b>{listing.address}</b> <br />
-									District: {listing.district} <br />
-									{/* <span onClick={handleUpdate(listing)}>{listing.price}</span> */}
-									Size: {listing.size} sqft
-									<br />
-									Price: ${listing.price}
-									<br />
-									{listing.no_of_bedrooms}{' '}
-									<img
-										src='http://cdn.onlinewebfonts.com/svg/img_391908.png'
-										height='20x'
-										width='20px'
-									/>
-									<br />
-									{listing.no_of_bathrooms}{' '}
-									<img
-										src='https://cdn-icons-png.flaticon.com/512/637/637270.png'
-										height='20x'
-										width='20px'
-									/>
-									<br />
-									<Link to={`/listings/${listing._id}`}>
-										<button className='viewListing'>
-											<span>View Listing</span>
+			{status === "No Watchlist" ? <div>no watchList</div> :<div>watchlist</div> } */}
+			{canList && (
+				<div className='listingList'>
+					<ul>
+						{watchlist.map((listing) => (
+							<li key={listing._id}>
+								<div className='listing'>
+									<div className='listingImage'>
+										{<img src={listing.image} height='300px' width='400px' />}
+									</div>
+									<div className='listingInfo'>
+										<b>{listing.address}</b> <br />
+										District: {listing.district} <br />
+										{/* <span onClick={handleUpdate(listing)}>{listing.price}</span> */}
+										Size: {listing.size} sqft
+										<br />
+										Price: ${listing.price}
+										<br />
+										{listing.no_of_bedrooms}{' '}
+										<img
+											src='http://cdn.onlinewebfonts.com/svg/img_391908.png'
+											height='20x'
+											width='20px'
+										/>
+										<br />
+										{listing.no_of_bathrooms}{' '}
+										<img
+											src='https://cdn-icons-png.flaticon.com/512/637/637270.png'
+											height='20x'
+											width='20px'
+										/>
+										<br />
+										<Link to={`/listings/${listing._id}`}>
+											<button className='viewListing'>
+												<span>View Listing</span>
+											</button>
+										</Link>
+										<Link to={`/listings/${listing._id}/edit`}>
+											<button className='viewListing'>
+												<span>Edit Listing</span>
+											</button>
+										</Link>
+										<button className='deleteListing'>
+											<span onClick={handleDelete(listing._id)}>Delete</span>
 										</button>
-									</Link>
-									<Link to={`/listings/${listing._id}/edit`}>
-										<button className='viewListing'>
-											<span>Edit Listing</span>
-										</button>
-									</Link>
-									<button className='deleteListing'>
-										<span onClick={handleDelete(listing._id)}>Delete</span>
-									</button>
+									</div>
 								</div>
-							</div>
-						</li>
-					))}
-				</ul>
-			</div>
-
-			{/* } */}
+							</li>
+						))}
+					</ul>
+				</div>
+			)}
 
 			<div>Suggested Listings</div>
 		</>
